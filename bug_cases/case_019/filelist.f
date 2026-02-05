9a5e12c075889bf13c2e8ccec241bd4fd79d533f
wolf-tungsten <gaoruihao@ict.ac.cn>
2026-02-05 11:17:00 +0800

    add C910 case_019 repro and fix report

---
+++ bug_cases/case_019/filelist.f
@@ -0,0 +1,3 @@
+./stub_modules.v
+../../C910_RTL_FACTORY/gen_rtl/clk/rtl/gated_clk_cell.v
+../../C910_RTL_FACTORY/gen_rtl/l2c/rtl/ct_l2c_data.v
---
+++ bug_cases/case_019/stub_modules.v
@@ -0,0 +1,2 @@
+// Minimal config define for ct_l2c_data parameterization.
+`define L2C_DATA_INDEX_WIDTH 13
